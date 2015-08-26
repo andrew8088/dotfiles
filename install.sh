d9dc78c80d0e19fa29ddf7741d27b4997e2bb745
Andrew Burgess <andrew@burgess.io>
2015-08-26 16:53:41 -0400

    typo in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -s "$PWD/zshrc" "$HOME/.zshrc"
 ln -s "$PWD/vimrc" "$HOME/.vimrc"
-ln -s "$PWD/.vim" "$HOME/vim"
+ln -s "$PWD/vim" "$HOME/.vim"
 
 ln -s "$PWD/zsh/pure/pure.zsh"  /usr/local/share/zsh/site-functions/prompt_pure_setup
 ln -s "$PWD/zsh/pure/async.zsh" /usr/local/share/zsh/site-functions/async
