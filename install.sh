c7290eaad0d6bea5904a8a210c2963a09d5b7cff
Andrew Burgess <andrew@burgess.io>
2016-04-15 16:52:50 -0400

    changed install.sh (far from complete, though)

--- install.sh
+++ install.sh
@@ -5,7 +5,4 @@ ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
 ln -s "$PWD/teamocil" "$HOME/.teamocil"
 ln -s "$PWD/gitignore" "$HOME/.gitignore"
 
-ln -s "$PWD/zsh/pure/pure.zsh"  /usr/local/share/zsh/site-functions/prompt_pure_setup
-ln -s "$PWD/zsh/pure/async.zsh" /usr/local/share/zsh/site-functions/async
-
 gem install teamocil
