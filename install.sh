b15a8d9994d76f79d40054339c122c323dc3f871
Andrew Burgess <andrew@burgess.io>
2020-08-03 15:17:49 -0400

    small tweaks

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ source ./homebrew-setup.sh
 ln -s "$PWD/zshrc" "$HOME/.zshrc"
 ln -s "$PWD/vimrc" "$HOME/.vimrc"
 ln -s "$PWD/vim" "$HOME/.vim"
+ln -s "$PWD/tmux" "$HOME/.tmux"
 ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
 ln -s "$PWD/teamocil" "$HOME/.teamocil"
 ln -s "$PWD/gitignore" "$HOME/.gitignore"
