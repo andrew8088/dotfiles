c4e9c8f76954a5e5804c1cdae601fbdfcdb1eef1
Andrew Burgess <andrew@burgess.io>
2020-10-07 22:27:14 -0400

    install tweaks (needs more work)

--- homebrew-setup.sh
+++ homebrew-setup.sh
@@ -1,11 +1,10 @@
 brew install trash
 brew install the_silver_searcher
-brew install tmux --head
 brew install tree
 brew install youtube-dl
 brew install jsonpp
-brew install neovim
 brew install nvm
 
 brew tap jakehilborn/jakehilborn
 brew install displayplacer
+brew install exa
