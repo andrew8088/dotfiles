5b00c4c84cd2d19fd871d9d25169a423f28c7543
Andrew Burgess <andrew.p.burgess@rbc.com>
2021-02-11 10:15:21 -0500

    add direnv to shell

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -49,7 +49,7 @@
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
-    # direnv                  # direnv status (https://direnv.net/)
+    direnv                  # direnv status (https://direnv.net/)
     # asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     # anaconda                # conda environment (https://conda.io/)
