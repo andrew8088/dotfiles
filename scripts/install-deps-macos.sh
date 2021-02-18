29e1da11f923ef426cca2b652c29c85bb9ee114e
Andrew Burgess <andrew@burgess.io>
2021-02-18 15:16:18 -0500

    add z to brew install list

--- scripts/install-deps-macos.sh
+++ scripts/install-deps-macos.sh
@@ -3,8 +3,8 @@ brew install the_silver_searcher
 brew install tree
 brew install youtube-dl
 brew install jsonpp
-brew install nvm
 
 brew tap jakehilborn/jakehilborn
 brew install displayplacer
 brew install exa
+brew install z
