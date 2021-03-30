42f103fc1ff531bf36a33247fe24fea67f2a62bd
Andrew Burgess <andrew@burgess.io>
2021-03-30 08:14:04 -0400

    clean up install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,14 +1,15 @@
+#!/usr/bin/zsh
 
- if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-     source ./install-deps-linux.sh
- elif [[ "$OSTYPE" == "darwin"* ]]; then
-     source ./install-deps-macos.sh
- fi
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    source ./install-deps-linux.sh
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    source ./install-deps-macos.sh
+fi
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.8.0
 
-. $HOME/.asdf/asdf.sh
+source $HOME/.asdf/asdf.sh
 
 asdf plugin add nodejs
 asdf install nodejs latest
