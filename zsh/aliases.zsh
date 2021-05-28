019e2e4ab6d6eeee577b5f9bc441065881fc49bb
Andrew Burgess <andrew.p.burgess@rbc.com>
2021-05-28 13:46:27 -0400

    docker alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -95,7 +95,10 @@ dclear () {
     docker ps -a -q | xargs docker kill -f
     docker ps -a -q | xargs docker rm -f
     docker images | grep "api\|none" | awk '{print $3}' | xargs docker rmi -f
+    docker volume prune -f
 }
+
+alias docker-clear=dclear
 
 dreset () {
     dclear
