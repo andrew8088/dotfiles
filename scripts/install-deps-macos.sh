5796dfb8319ecb963888ebcf03a06adcf53a01aa
Andrew Burgess <andrew.burgess@classdojo.com>
2021-06-15 13:35:22 +0000

    updated install scripts

--- scripts/install-deps-macos.sh
+++ scripts/install-deps-macos.sh
@@ -1,3 +1,16 @@
+brew tap federico-terzi/espanso
+brew install espanso
+brew install koekeishiya/formulae/yabai
+brew install obsidian
+brew install fzf
+brew install zsh-syntax-highlighting
+brew install firefox
+brew install skhd
+brew install istat-menus
+brew install docker
+brew install docker-compose
+brew install cask slack
+brew install alfred
 brew install trash
 brew install the_silver_searcher
 brew install tree
@@ -8,3 +21,9 @@ brew tap jakehilborn/jakehilborn
 brew install displayplacer
 brew install exa
 brew install z
+
+# spark, fantastical, cardhop, shush, kaleidoscope
+
+brew install ksdiff
+brew install whatsapp
+brew install alt-tab
--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/zsh
+#!/bin/zsh
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    source ./install-deps-linux.sh
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    source ./install-deps-macos.sh
-fi
+#if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#    source ./install-deps-linux.sh
+#elif [[ "$OSTYPE" == "darwin"* ]]; then
+#    source ./install-deps-macos.sh
+#fi
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.8.0
@@ -12,7 +12,9 @@ git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.8.0
 source $HOME/.asdf/asdf.sh
 
 asdf plugin add nodejs
-asdf install nodejs latest
+NODEJS_CHECK_SIGNATURES=no asdf install nodejs 14.17.0
 asdf global nodejs $(asdf list nodejs | tail -1 | sed 's/^ *//g')
 
 npm install -g git-branch-utils
+npm install -g neovim
+
