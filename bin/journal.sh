8f163d3962f294b0955d40006e5ef79deb7f2fdb
Andrew Burgess <andrew@burgess.io>
2021-08-18 21:46:23 -0400

    updated journal.sh

--- bin/journal.sh
+++ bin/journal.sh
@@ -2,15 +2,32 @@
 
 set -e
 
+if [[ -z "$NOTES_DIR" ]]; then
+    echo "NOTES_DIR is not set"
+    exit 1
+fi
+
+if [[ -z "$JOURNAL_DIR" ]]; then
+    echo "JOURNAL_DIR is not set"
+    exit 1
+fi
+
 cd "$NOTES_DIR"
 
-echo "<body>"
+html="$JOURNAL_DIR/index.html"
+
+echo """<head>
+<title>Journal</title>
+<meta charset="UTF-8" />
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>""" > "$html"
 ag --files-with-matches "#journal" | sort | while read line
 do
-    echo "<article>"
-    echo "<h1>$(date -jf "%Y%m%d%H%M.md" "$line" +"%A %B %d, %Y")</h1>"
-    echo "<h2>$(date -jf "%Y%m%d%H%M.md" "$line" +"%H:%M")</h2>"
-    cat "$line" | grep -v "#journal" | markdown
-    echo "</article>"
+    echo "<article>" >> "$html"
+    echo "<h1>$(date -jf "%Y%m%d%H%M.md" "$line" +"%A %B %d, %Y")</h1>" >> "$html"
+    echo "<h2>$(date -jf "%Y%m%d%H%M.md" "$line" +"%H:%M")</h2>" >> "$html"
+    cat "$line" | grep -v "#journal" | markdown >> "$html"
+    echo "</article>" >> "$html"
 done
-echo "</body>"
+echo "</body>" >> "$html"
