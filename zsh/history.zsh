c724cbf4dcda25a2c7ecc1e2bb67d79b23fb500c
Andrew Burgess <andrew.burgess@classdojo.com>
2021-08-30 08:04:49 -0400

    zsh tweaks

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -38,4 +38,4 @@ setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
-setopt share_history          # share command history data
+# setopt share_history          # share command history data
