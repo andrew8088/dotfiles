3739e6c9ce783dc3e228e04a8e14174452625a78
Andrew Burgess <andrew.burgess@classdojo.com>
2022-01-24 10:04:27 -0500

    update sync-notes script

--- bin/sync-notes.sh
+++ bin/sync-notes.sh
@@ -1,8 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 
 # Usage via cron:
 #
-# */5 * * * * NOTES_DIR=/path/to/notes /path/to/sync-notes.sh >> /tmp/sync-notes.log
+# */5 * * * * NOTES_DIR=/path/to/notes /path/to/sync-notes.sh >> /tmp/sync-notes.log 2>&1
+
+set -ex
 
 if [[ -z "$NOTES_DIR" ]]; then
     echo "NOTES_DIR is not set"
@@ -12,11 +14,6 @@ fi
 cd "$NOTES_DIR"
 
 git pull --ff-only
-
-git add .obsidian*
-git commit -m 'obsidian config'
-
 git add .
-git commit -m "$(date '+%F %T')"
-
+git commit -m "$(date '+%F %T')" | tee >(cat)
 git push
