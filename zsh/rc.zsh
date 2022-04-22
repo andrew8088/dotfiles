d26f27d42f1d1e6d5eb22939b6f1a475c535a67c
Andrew Burgess <andrew@burgess.io>
2022-04-22 08:17:14 -0400

    prompt config

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -16,8 +16,8 @@ source_if_exists $DOTFILES/zsh/history.zsh
 source_if_exists $DOTFILES/zsh/git.zsh
 source_if_exists ~/.fzf.zsh
 source_if_exists $DOTFILES/zsh/aliases.zsh
-source_if_exists "$DOTFILES/zsh/p10k.zsh"
-source_if_exists $HOME/powerlevel10k/powerlevel10k.zsh-theme
+# source_if_exists "$DOTFILES/zsh/p10k.zsh"
+# source_if_exists $HOME/powerlevel10k/powerlevel10k.zsh-theme
 # source_if_exists $HOME/.asdf/asdf.sh
 source_if_exists /usr/local/etc/profile.d/z.sh
 source_if_exists /opt/homebrew/etc/profile.d/z.sh
