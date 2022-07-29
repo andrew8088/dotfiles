21c35dbce17c4090a9740fc0c1d346bf595787ad
Andrew Burgess <andrew@burgess.io>
2022-07-29 08:44:59 -0400

    fix yt.sh padding command

--- scripts/yt.sh
+++ scripts/yt.sh
@@ -3,13 +3,13 @@
 filepath="/tmp/youtube-mode"
 
 if [[ $(cat "$filepath") == *"1"* ]]; then
-  yabai -m space code --padding abs:10:10:10:10
-  yabai -m space code --gap abs:10
+  yabai -m space --padding abs:10:10:10:10
+  yabai -m space --gap abs:10
   osascript -e 'tell application "System Events" to set autohide menu bar of dock preferences to false'
   echo "0" > "$filepath"
 else
-  yabai -m space code --padding abs:0:0:0:0
-  yabai -m space code --gap abs:0
+  yabai -m space --padding abs:0:0:0:0
+  yabai -m space --gap abs:0
   osascript -e 'tell application "System Events" to set autohide menu bar of dock preferences to true'
   echo "1" > "$filepath"
 fi
