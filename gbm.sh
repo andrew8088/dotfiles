968355910832cc8168100684eedb0b36c7248b80
Andrew Burgess <andrew@burgess.io>
2022-08-12 20:40:50 -0400

    wip

--- gbm.sh
+++ gbm.sh
@@ -2,34 +2,41 @@
 
 # add check: is this git repo?
 
-command=$(gum choose rebase delete update)
+gum style \
+  --border normal \
+  --margin "1" \
+  --padding "1 2" \
+  --border-foreground "" "Hello, there! Welcome to $(gum style --foreground 212 'Gum')."
 
-get_branches () {
+# command=$(gum choose rebase delete update)
 
-  if [ ${var+x} ]; then
-    gum choose --limit="$1" $(git branch --format="%(refname:short)")
-  else 
-    gum choose --no-limit $(git branch --format="%(refname:short)")
-  fi
-}
+# get_branches () {
 
-get_branches | while read -r branch
-do
-  git checkout "$branch"
+#   if [ ${var+x} ]; then
+#     gum choose --limit="$1" $(git branch --format="%(refname:short)")
+#   else 
+#     gum choose --no-limit $(git branch --format="%(refname:short)")
+#   fi
+# }
 
-  case $command in
-    rebase)
-      base_branch=$(get_branches 1)
-      git fetch origin
-      git rebase "origin/$base_branch"
-      ;;
-    delete)
-      echo "deleting is easy"
-      ;;
-    update)
-      echo "updating is okay"
-      ;;
-  esac
-done
+# get_branches | while read -r branch
+# do
+#   # git checkout "$branch"
+
+#   case $command in
+#     rebase)
+#       # base_branch=$(get_branches 1)
+#       # git fetch origin
+#       # git rebase "origin/$base_branch"
+#       echo "rebase is hard"
+#       ;;
+#     delete)
+#       echo "deleting is easy"
+#       ;;
+#     update)
+#       echo "updating is okay"
+#       ;;
+#   esac
+# done
 
 
