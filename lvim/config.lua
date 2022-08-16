0d1e97082a1b3fa7a2249b4fbec14fb4940542bd
Andrew Burgess <andrew@burgess.io>
2022-08-16 08:14:50 -0400

    lvim cleanup

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,13 +1,3 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
