f349571b81b6eafd6de9715c00d21023a855bf1a
Andrew Burgess <andrew@burgess.io>
2022-08-23 06:20:51 -0400

    .ledger: format on save

--- lvim/config.lua
+++ lvim/config.lua
@@ -166,6 +166,6 @@ lvim.plugins = {
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
--- { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
+lvim.autocommands.custom_groups = {
+  { "BufWritePre", "*.ledger", "LedgerAlignBuffer" },
+}
