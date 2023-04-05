9f2ff41d8391f67d6f44d7dd61bfd0d4d049468f
Andrew Burgess <andrew.burgess@classdojo.com>
2023-04-04 21:31:38 -0400

    add macos deps and update alacritty with new font

--- install/install-deps-macos.sh
+++ install/install-deps-macos.sh
@@ -1,6 +1,6 @@
-brew tap federico-terzi/espanso
-brew install espanso
-brew install koekeishiya/formulae/yabai
+# brew tap federico-terzi/espanso
+# brew install espanso
+# brew install koekeishiya/formulae/yabai
 brew install obsidian
 brew install fzf
 brew install zsh-syntax-highlighting
@@ -9,17 +9,17 @@ brew install skhd
 brew install istat-menus
 brew install docker
 brew install docker-compose
-brew install cask slack
+# brew install cask slack
 brew install alfred
 brew install trash
 brew install the_silver_searcher
 brew install tree
-brew install youtube-dl
+brew install yt-dlp
 brew install jsonpp
 brew install jq
 
-brew tap jakehilborn/jakehilborn
-brew install displayplacer
+# brew tap jakehilborn/jakehilborn
+# brew install displayplacer
 brew install exa
 brew install z
 
@@ -27,7 +27,11 @@ brew install z
 
 brew install ksdiff
 brew install whatsapp
-brew install alt-tab
+# brew install alt-tab
 brew install neovim
-brew install alacritty
-brew install z
+# brew install alacritty
+
+brew install fnm
+brew install tmux
+brew install rust
+brew install --cask rectangle
