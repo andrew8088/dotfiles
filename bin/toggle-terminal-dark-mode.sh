9d28cf7989d1aa047db65dc65a0c3ae6a283e7ed
Andrew Burgess <andrew.burgess@classdojo.com>
2024-07-23 21:52:02 +0000

    tweak terminal dark-mode script

--- bin/toggle-terminal-dark-mode.sh
+++ bin/toggle-terminal-dark-mode.sh
@@ -2,24 +2,27 @@
 
 set -e
 
+DARK_THEME=mocha
+LIGHT_THEME=latte
+
 cd $DOTFILES
 
-DARK_COUNT="$(ag --files-with-matches --ignore-dir=./bin --nobreak "catppuccin.*mocha"  | wc -l | xargs)"
-LIGHT_COUNT="$(ag --files-with-matches --ignore-dir=./bin --nobreak "catppuccin.*latte" | wc -l | xargs)"
+DARK_COUNT="$(ag --files-with-matches --ignore-dir=./bin --nobreak "catppuccin.*$DARK_THEME"  | wc -l | xargs)"
+LIGHT_COUNT="$(ag --files-with-matches --ignore-dir=./bin --nobreak "catppuccin.*$LIGHT_THEME" | wc -l | xargs)"
 
 if [ $DARK_COUNT -eq 4 ]; then
   # go light
-  ag --files-with-matches --ignore-dir=./bin --nobreak "catppuccin.*mocha" | while read file; do
-    gsed -i 's/mocha/latte/' "$file"
+  ag --files-with-matches --ignore-dir=./bin --nobreak "catppuccin.*$DARK_THEME" | while read file; do
+    gsed -i "s/$DARK_THEME/$LIGHT_THEME/" "$file"
   done
   tmux source-file ~/.tmux.conf
 elif [ $LIGHT_COUNT -eq 4 ]; then
   # go dark
-  ag --files-with-matches --ignore-dir=./bin --nobreak "catppuccin.*latte" | while read file; do
-    gsed -i 's/latte/mocha/' "$file"
+  ag --files-with-matches --ignore-dir=./bin --nobreak "catppuccin.*$LIGHT_THEME" | while read file; do
+    gsed -i "s/$LIGHT_THEME/$DARK_THEME/" "$file"
   done
   tmux source-file ~/.tmux.conf
 else
-  echo "expected 3 dark or 3 light lines, but found $DARK_COUNT dark and $LIGHT_COUNT light lines"
+  echo "expected 4 dark or 4 light lines, but found $DARK_COUNT dark and $LIGHT_COUNT light lines"
   exit 1
 fi
