0817d88f46429964dfd5ed3c8c3ca7c463991f35
Andrew Burgess <andrew@burgess.io>
2025-06-17 12:41:43 +0000

    remove ai

--- nvim/lua/plugins/formatter.lua
+++
@@ -1,4 +0,0 @@
--- Removed: functionality moved to LSP-based formatting in lspconfig.lua
--- This simplifies the configuration by using built-in LSP formatting capabilities
--- instead of maintaining a separate formatter plugin
-return {}
